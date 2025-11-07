708764852b8d7de9957a95f3ee94e4d380273e2f
patrice-dev <237076804+patrice-dev@users.noreply.github.com>
2025-11-06 20:54:35 -0500

    fix: update file path references in testScript.py for consistency

--- testScript.py
+++ testScript.py
@@ -1,10 +1,10 @@
 import os
 
 def test_file_exists():
-    assert os.path.exists("index.html"), "index.html est manquant!"
+    assert os.path.exists("./index.html"), "index.html est manquant!"
 
 def test_contains_nom_etudiant():
-    with open("index.html", "r") as file:
+    with open("./index.html", "r") as file:
         content = file.read()
         assert "<span id=\"nom_etudiant\">" in content, "Le champ nom_etudiant est manquant!"
 
